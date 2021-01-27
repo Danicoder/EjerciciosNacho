3fc20883571057163fbfc07ac56c909cc559eab1
Danicoder <56139050+Danicoder@users.noreply.github.com>
2021-01-27 19:12:01 +0100

    Arrays
    Comiezos de almacenamiento

---
+++ Arrays/Arrays/Arrays/Exercices.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    class Exercices
+    {
+        /*(4.1.1.1) Un programa que pida al usuario 4 números, los memorice (utilizando un
+          array), calcule su media aritmética y después muestre en pantalla la media y los
+          datos tecleados.*/
+        public static float Example4111(int numero1,int numero2,int numero3)
+        {   
+            int[] numeros = new int[numero1];
+
+                numeros[0] = numero1;
+                numeros[1] = numero2;
+                numeros[2] = numero3;
+
+            float result = (numeros[0] + numeros[1] + numeros[2])/3;
+            return result;
+        }
+        /*(4.1.1.2) Un programa que pida al usuario 5 números reales (pista: necesitarás un
+        array de "float") y luego los muestre en el orden contrario al que se introdujeron.*/
+        public static void Example4112()
+        {
+
+        }
+    }
+}
---
+++ Arrays/Arrays/Arrays/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        public static void ReadNumber()
+        {
+            try
+            {
+                Console.WriteLine("Escribe un número: ");
+                float Opcion = Convert.ToInt64(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Error, vuelve a escribir un número");
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Introduce 1 números");
+                int Option0 =Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Introduce otro números");
+                int Option1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Introduce otro números");
+                int Option2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Los números introducidos han sido: "+Option0+" "+Option1+" "+Option2+" ");
+                Console.WriteLine("La media aritmética es: "+ Exercices.Example4111(Option0,Option1,Option2));
+            }
+            catch
+            {
+                Console.WriteLine("Introduce 3 números, NO MÁS");
+            }
+
+            for(int i=0;i >= 5; i++)
+            {
+                ReadNumber();
+            }
+            
+        }
+    }
+}
